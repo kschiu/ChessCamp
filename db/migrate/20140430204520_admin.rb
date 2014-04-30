6df7a1634c1c54c8d86fa8a492792fd18b9e325b
Kevin Chiu <kschiu@andrew.cmu.edu>
2014-04-30 20:53:35 +0000

    Added profh to the system

---
+++ db/migrate/20140430204520_admin.rb
@@ -0,0 +1,16 @@
+class Admin < ActiveRecord::Migration
+  def up
+    admin = User.new
+    admin.username = "profh"
+    admin.instructor_id = 12
+    admin.password = "secret"
+    admin.password_confirmation = "secret"
+    admin.role = "admin"
+    admin.save!
+  end
+
+  def down
+    admin = User.find_by_username "profh"
+    User.delete admin
+  end
+end
