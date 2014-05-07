5c2c1d2b9ec25c83df0b7bb121fea92425eb94e5
Kevin Chiu <kschiu@andrew.cmu.edu>
2014-05-07 16:03:23 +0000

    80% of Admin dashboard complete. Working on Callout box to add registration on admin Dashboard. Other things to be completed: Put in Map, Progress Bar for capacity, Fix tables centering and Fix delete messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
   	@upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(5)
-    @past_camps = Camp.past.chronological.paginate(:page => params[:page]).per_page(5)
     @active_locations = Location.active.alphabetical.paginate(:page => params[:page]).per_page(5)
+    @active_curriculums = Curriculum.active.alphabetical.paginate(:page => params[:page]).per_page(5)
   end
 
   def about
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,52 @@
 class RegistrationsController < ApplicationController
+  include ActionView::Helpers::NumberHelper
+  before_action :set_registration, only: [:show, :edit, :update, :destroy]
+  authorize_resource
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @registration = Registration.new
+  end
+
+  def edit
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+    if @registration.save
+      redirect_to @registration, notice: "Registration was added to the system."
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @registration.update(registration_params)
+      redirect_to @registration, notice: "Registration was revised in the system."
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @registration.destroy
+    redirect_to registrations_url, notice: "Registration was removed from the system."
+  end
+
+  private
+    def set_registration
+      @registration = Registration.find(params[:id])
+    end
+
+    def registration_params
+      params.require(:registration).permit(:camp_id, :student_id, :payment_status, :points_earned)
+    end
+end
+
 
 end
