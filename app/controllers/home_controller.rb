4d0213dc0b101cb8847d4da3132621f9f38b3fa1
Kevin Chiu <kschiu@andrew.cmu.edu>
2014-05-07 05:05:55 +0000

    Working on Admin Dashboard... Has a Camps and Locations window. also box with user and quick links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	@upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(5)
     @past_camps = Camp.past.chronological.paginate(:page => params[:page]).per_page(5)
-    @inactive_camps = Camp.inactive.alphabetical.to_a
+    @active_locations = Location.active.alphabetical.paginate(:page => params[:page]).per_page(5)
   end
 
   def about
