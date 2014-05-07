63d7ddeeb142d6f6516ac4ad40413bae97c6d1a3
Kevin Chiu <kschiu@andrew.cmu.edu>
2014-05-07 03:40:17 +0000

    working on admin dashboard...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+  	@upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(5)
+    @past_camps = Camp.past.chronological.paginate(:page => params[:page]).per_page(5)
+    @inactive_camps = Camp.inactive.alphabetical.to_a
   end
 
   def about
