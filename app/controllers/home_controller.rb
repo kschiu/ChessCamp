bd3296744e424ca113afdbe594ff4f70cda70e6d
Kevin Chiu <kschiu@andrew.cmu.edu>
2014-05-08 03:14:55 +0000

    final commit. Made some tweaks to views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   	@upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(5)
     @active_locations = Location.active.alphabetical.paginate(:page => params[:page]).per_page(5)
     @active_curriculums = Curriculum.active.alphabetical.paginate(:page => params[:page]).per_page(5)
-    @student = Student.all
     if logged_in? && current_user.role =='instructor'
       @instructor_camps = Instructor.find(current_user.instructor_id).camps.upcoming.chronological.paginate(:page => params[:page]).per_page(5)
       @instructor_students = Instructor.find(current_user.instructor_id).camps.first.students.paginate(:page => params[:page]).per_page(5)
